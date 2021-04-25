6d9ddb682b209e11b89b07e9004b030efb3d70a6
Liza Pressman <lizarogot@gmail.com>
2021-04-25 12:50:37 -0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typical from "react-typical";
 import { Icon } from '@iconify/react';
 import bxPaint from '@iconify-icons/bx/bx-paint';
+import Button from 'react-bootstrap/esm/Button';
 
 
 export const Header = () => {
@@ -18,18 +19,25 @@ export const Header = () => {
     // }
 
     const HeaderTitleTypeAnimation = React.memo(() => {
-        return <Typical className="title-styles" steps={["SENIOR STUDIO ART MAJOR", 1500, "PAINTER", 1500, "SCULPTOR", 1500, "DEVELOPER", 1500]} loop={50} />
+        return (
+            <div>
+                <Typical className="title-styles" steps={[1500, 1500, "I'm a painter", 1500, "I'm a sculptor", 1500, "I'm a developer - ", 2800, "PAINTER"]} />
+                <Typical className="title-styles" steps={[1500, 1500, 1500, 1500, 1500, 1500, "I even made this website!", 2800, "SCULPTOR"]} />
+                <Typical className="title-styles" steps={[1500, 1500, 1500, 1500, 1500, 1500, 1500, "Click around a bit :)", 2800, "DEVELOPER"]} />
+            </div >
+        )
     }, (props, prevProp) => true);
 
     return (
         <header id="home" style={{ height: window.innerHeight - 70, display: 'block' }}>
-            <div className="row aligner" style={{ height: '100%' }}>
+            <div className="row aligner" style={{ height: '50%' }}>
                 <div className="col-md-12">
                     <div>
                         <Icon icon={bxPaint} className="iconify header-icon" />
                         <br />
                         <h1 className="mb-0">
-                            <Typical steps={["LIZA PRESSMAN"]} wrapper="p" />
+                            {/* <Typical steps={["LIZA PRESSMAN"]} wrapper="p" /> */}
+                            <Typical steps={["Hi, I'm Liza"]} wrapper="p" />
                         </h1>
                         <div className="title-container">
                             <HeaderTitleTypeAnimation />
